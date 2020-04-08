cb6b6a7cb1506c016cb8c721e1b33b8feadbd4a9
Edwin Dos Santos <31744511+EdwinSantos@users.noreply.github.com>
2020-04-07 22:36:43 -0400

    Cleaned up some of the code and the output.

---
+++ RunMeTheFirstTime.bat
@@ -0,0 +1,5 @@
+runas /user:Administrator
+python get-pip.py
+pip install bs4
+pip install pandas
+pip install re
--- scrapper.py
+++ scrapper.py
@@ -4,69 +4,76 @@ import pandas
 import re
 
 
-def scrapeFrom411(streetName):
-    streetName = streetName.replace(" ", "+")
-    url = 'https://www.canada411.ca/search/si/1/-/' + streetName + '+Brampton+ON/?pgLen=50'
-    htmlContent = requests.get(url)
-    content = BeautifulSoup(htmlContent.content, "html.parser")
+def scrape_from_411(street_name):
+    street_name = street_name.replace(" ", "+")
+    url = 'https://www.canada411.ca/search/si/1/-/' + street_name + '+Brampton+ON/?pgLen=100'
+    html_content = requests.get(url)
+    content = BeautifulSoup(html_content.content, "html.parser")
 
     names = []
-    phoneNumbers = []
-    streetNames = []
-    houseNumbers = []
+    phone_numbers = []
+    street_names = []
+    house_numbers = []
 
     for Name in content.findAll('h2', attrs={"class": "c411ListedName"}):
         names.append(Name.text)
 
     for PhoneNumber in content.findAll('span', attrs={"class": "c411Phone"}):
-        phoneNumbers.append(PhoneNumber.text)
+        phone_numbers.append(PhoneNumber.text)
 
     for Address in content.findAll('span', attrs={"class": "adr"}):
-        houseNumbers.append(int(re.search("\w*(?= )", Address.text).group(0)))
-        streetNames.append(re.search("\s(.*)", Address.text).group(0))
+        house_numbers.append(int(re.search("\w*(?= )", Address.text).group(0)))
+        street_names.append(re.search("\s(.*)", Address.text).group(0))
 
-    df = pandas.DataFrame(data={"House Number": houseNumbers, "Street Name": streetNames, "Phone Number": phoneNumbers,
-                                "Name": names})
+    df = pandas.DataFrame(data={"House Number": house_numbers, "Street Name": street_names,
+                                "Phone Number": phone_numbers, "Name": names})
     df = df.sort_values(by='House Number')
     return df
 
 
 def getInfoFromConsole():
     streetName = input("Enter the street name\n")
-    df = scrapeFrom411(streetName)
+    df = scrape_from_411(streetName)
     df.to_csv(streetName + ".csv", sep=',', index=False)
 
 
-def getInfoFromFile():
-    fileName = input("Enter the file name in the format MyTerritory.csv\n")
-    originalDataFrame = pandas.read_excel(fileName, skiprows=11)
-    address = originalDataFrame.iloc[:, :3]
-    address.columns = ["House Number", "Street Name", "Symbol"]
-    indexNames = address[address["Symbol"].isin(["JW", "DNC", "NE"])].index
-    address = address.drop(indexNames, inplace=False)
-    phonebooksResults = pandas.DataFrame()
-    # Get unique values of streets
-    uniqueStreets = address["Street Name"].unique()
-    for street in uniqueStreets:
-        housesOnStreet = address.loc[address["Street Name"] == street]
-        tempDF = pandas.merge(housesOnStreet, scrapeFrom411(street), how="left", on=["House Number", "House Number"])
-        phonebooksResults = phonebooksResults.append(tempDF)
+def get_info_from_file():
+    # Read the original territory and just keep the info that's important
+    file_name = input("Enter the file name in the format MyTerritory.csv\n")
+    original_territory = pandas.read_excel(file_name, skiprows=11)
+    original_territory_address = original_territory.iloc[:, :3]
+    original_territory_address.columns = ["House Number", "Street Name", "Symbol"]
+    # Get rid of DNC, Witnesses ect.
+    symbols_to_drop = original_territory_address[original_territory_address["Symbol"].isin(["JW", "DNC", "NE"])].index
+    original_territory_address = original_territory_address.drop(symbols_to_drop, inplace=False)
+    final_territory = pandas.DataFrame()
+    # Send the individual streets to the scrapper
+    unique_streets = original_territory_address["Street Name"].unique()
+    for street in unique_streets:
+        houses_on_street = original_territory_address.loc[original_territory_address["Street Name"] == street]
+        joined_tables = pandas.merge(houses_on_street, scrape_from_411(street), how="left", on=["House Number", "House "
+                                                                                                              "Number"])
+        final_territory = final_territory.append(joined_tables)
     # output the results from the scrape to the territory
-    phonebooksResults = phonebooksResults[phonebooksResults['Phone Number'].notna()]
-    phonebooksResults.to_csv(fileName + "WithPhoneNumbers.csv", sep=',', index=False)
+    final_territory = final_territory[final_territory['Phone Number'].notna()]
+    final_territory = final_territory.drop(["Symbol", "Street Name_x"], 1)
+    final_territory.to_csv(file_name + "WithPhoneNumbers.csv", sep=',', index=False)
 
 
-def getInfoFromOnlineTerritory():
+def get_info_from_online_territory():
     # Havent setup to handle online territory
-    return 1
+    print("This is still under construction")
+    exit()
 
 
 def main():
-    inputFormat = input("Get phone numbers from \n 1. Territory on the web \n 2. Territory in file \n 3. Street\n")
+    inputFormat = input("Get phone numbers from \n 1. Territory on the web \n 2. Territory in file \n 3. Street\n eg. "
+                        "if you want to use a territory you have saved as a file enter the number 2 on the line below"
+                        " \n")
     if inputFormat == "1":
-        getInfoFromOnlineTerritory()
+        get_info_from_online_territory()
     elif inputFormat == "2":
-        getInfoFromFile()
+        get_info_from_file()
     elif inputFormat == "3":
         getInfoFromConsole()
     else:
