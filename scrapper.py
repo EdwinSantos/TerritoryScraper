9a2ac9d5ed335be2e415ee1d87552b957104a3b9
Edwin Dos Santos <31744511+EdwinSantos@users.noreply.github.com>
2020-04-18 18:58:23 -0400

    Optimized the whitepages lookup and cleaned up the code base for 411 lookup

--- scrapper.py
+++ scrapper.py
@@ -22,7 +22,7 @@ street_dict = {
 }
 
 
-def scrape_from_WhitePagesCanada(street_name):
+def scrape_from_WhitePagesCanada(street_name, houses_on_street, whole_street):
     print("Looking up people on" + street_name)
 
     # Fix street type because white pages is trash
@@ -55,8 +55,10 @@ def scrape_from_WhitePagesCanada(street_name):
             if "No record found" in page.text:
                 collected_all_pages = True
                 break
-        for person in content.findAll('a', attrs={"class": "rsslink-m"}):
-            person_url.append(person.get("href"))
+        for person in content.findAll('a', attrs={"style": "color:#333; font-size:16px; text-decoration:none"}):
+            house_number = int(re.search("\w*(?= )", person.text).group(0))
+            if (house_number in houses_on_street) or whole_street:
+                person_url.append(person.get("href"))
         counter = counter + 1
     people = list()
     print("Collecting all of the personal information")
@@ -67,16 +69,14 @@ def scrape_from_WhitePagesCanada(street_name):
         telephone = content.find('span', attrs={"itemprop": "telephone"})
         address = content.find('span', attrs={"itemprop": "streetAddress"})
         name = content.find('span', attrs={"itemprop": "name"})
-        house_number = int(re.search("\w*(?= )", address.text).group(0))
-        street_name = re.search("\s(.*)", address.text).group(0)
+        house_number, street_name = split_address(address.text)
         people.append([house_number, street_name, telephone.text, name.text])
 
     scrape_results = pandas.DataFrame.from_records(people, columns=["House Number", "Address", "Phone Number", "Name"])
     return scrape_results
 
 
-# Clean up to use the standard data formatting used by WhitePages
-def scrape_from_411(street_name):
+def scrape_from_411(street_name, houses_on_street, whole_street):
     print("Looking up " + street_name + " on 411")
     street_name = street_name.replace(" ", "+")
     url = 'https://www.canada411.ca/search/si/1/-/' + street_name + '+Brampton+ON/?pgLen=100'
@@ -84,34 +84,31 @@ def scrape_from_411(street_name):
     html_content = requests.get(url)
     content = BeautifulSoup(html_content.content, "html.parser")
 
-    names = []
-    phone_numbers = []
-    street_names = []
-    house_numbers = []
+    people = list()
 
-    for Name in content.findAll('h2', attrs={"class": "c411ListedName"}):
-        names.append(Name.text)
+    for FullPerson in content.findAll('div', attrs={"class": "c411Listing jsResultsList"}):
+        phone_number = FullPerson.find('span', attrs={"class": "c411Phone"}).text
+        name = FullPerson.find('h2', attrs={"class": "c411ListedName"}).string
+        full_address = FullPerson.find('span', attrs={"class": "adr"}).text
+        short_address = full_address.partition(" Brampton")[0]
+        house_number, street_name = split_address(short_address)
+        if (house_number in houses_on_street) or whole_street:
+            people.append([house_number, street_name, phone_number, name])
+    scrape_results = pandas.DataFrame.from_records(people, columns=["House Number", "Address", "Phone Number", "Name"])
+    return scrape_results
 
-    for PhoneNumber in content.findAll('span', attrs={"class": "c411Phone"}):
-        phone_numbers.append(PhoneNumber.text)
 
-    for Address in content.findAll('span', attrs={"class": "adr"}):
-        house_numbers.append(int(re.search("\w*(?= )", Address.text).group(0)))
-        short_address = Address.text.partition(" Brampton")[0]
-        street_names.append(re.search("\s(.*)", short_address).group(0))
-
-    df = pandas.DataFrame(data={"House Number": house_numbers, "Address": street_names,
-                                "Phone Number": phone_numbers, "Name": names})
-    return df
+def split_address(address):
+    house_number = int(re.search("\w*(?= )", address).group(0))
+    street_name = re.search("\s(.*)", address).group(0)
+    return house_number, street_name
 
 
 def getInfoFromConsole():
     streetName = input("Enter the street name\n")
-    scrape_from_411_results = scrape_from_411(streetName)
-    scrape_from_whitepagescanada_results = scrape_from_WhitePagesCanada(streetName)
+    scrape_from_411_results = scrape_from_411(streetName, [], True)
+    scrape_from_whitepagescanada_results = scrape_from_WhitePagesCanada(streetName, [], True)
     scraped_tables = pandas.concat([scrape_from_411_results, scrape_from_whitepagescanada_results])
-    print("Concatinated Tables")
-    print(scraped_tables)
     scraped_tables = scraped_tables.drop_duplicates(subset=["House Number", "Phone Number"])
     final_df = scraped_tables.sort_values("House Number")
     # Scrape from both and send the results to another method to do the merging
@@ -140,21 +137,18 @@ def get_info_from_file():
     # Send the individual streets to the scrapper
     unique_streets = original_territory_address["Street Name"].unique()
     for street in unique_streets:
-        houses_on_street = original_territory_address.loc[original_territory_address["Street Name"] == street]
-        df_canada_white_pages = scrape_from_WhitePagesCanada(street)
-        df_411 = scrape_from_411(street)
+        all_entries_on_street = original_territory_address.loc[original_territory_address["Street Name"] == street]
+        houses_on_street = all_entries_on_street["House Number"].tolist()
+
+        df_canada_white_pages = scrape_from_WhitePagesCanada(street, houses_on_street, False)
+        df_411 = scrape_from_411(street, houses_on_street, False)
         scraped_tables = pandas.concat([df_canada_white_pages, df_411])
-        print("Concatinated Tables")
-        print(scraped_tables)
         scraped_tables = scraped_tables.drop_duplicates(subset=["House Number", "Phone Number"])
         print("Removed the duplicates")
-        print(scraped_tables)
-        joined_tables = pandas.merge(houses_on_street, scraped_tables, how="left", on=["House Number", "House Number"])
-        joined_tables_sorted = joined_tables.sort_values("House Number")
-        final_territory = final_territory.append(joined_tables_sorted)
+        scraped_tables_sorted = scraped_tables.sort_values("House Number")
+        final_territory = final_territory.append(scraped_tables_sorted)
     # output the results from the scrape to the territory
     final_territory = final_territory[final_territory['Phone Number'].notna()]
-    final_territory = final_territory.drop(["Symbol", "Address"], 1)
     final_territory.to_csv(full_file_name + "WithPhoneNumbers.csv", sep=',', index=False)
 
 
@@ -174,11 +168,9 @@ def main():
         get_info_from_file()
     elif inputFormat == "3":
         getInfoFromConsole()
-    elif inputFormat == "0":
-        # Debug mode
-        scrape_from_WhitePagesCanada("Montjoy")
     else:
         print("Please use one of the standard inputs")
+        main()
 
 
 if __name__ == "__main__": main()
